ed3611e3e2a09a3ebd76bb79653185eafd15f1ac
rishikhurana2 <league@WTS7.attlocal.net>
2017-05-22 20:04:16 -0700

    For loop gauntlet
    05-22-2017

---
+++ src/for_loop_gauntlet.java
@@ -0,0 +1,44 @@
+import javax.util.*;
+
+public class for_loop_gauntlet{
+	public static void main(String[] args) {
+		for(int i=0; i<=100; i++){
+			System.out.println(i);
+			
+
+			
+			
+			
+			
+			
+		}
+		for(int k=100; k>=0; k--){
+		System.out.println(k);	
+		}
+		for(int j=2; j<=100; j=j+2){
+			System.out.println(j);
+		}
+			for(int f=1; f<=99; f=f+2){
+				System.out.println(f);
+			}
+			for(int a=1; a<=500; a++){
+				if (a%2==0) {
+					System.out.println(a+" even");}
+					else {
+						System.out.println(a+" odd");
+					}
+				}
+			for(int p=0; p<=777; p=p+7){
+				System.out.println(p);
+			}
+			int h=0;
+			for(int t=2000; t<=2016; t++){
+				h=t-2000;
+				System.out.println("In "+ t +", i was "+ h +" years old.");
+					
+			}
+			
+		}
+	}
+		
+	
